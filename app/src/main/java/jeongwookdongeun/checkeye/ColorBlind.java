8fab02f3a1abb480dd6d8fad4de0637ba2da09b8
ddongni <de0309@naver.com>
2022-01-23 17:25:19 +0900

    색맹 테스트 추가

---
+++ app/src/main/java/jeongwookdongeun/checkeye/ColorBlind.java
@@ -0,0 +1,35 @@
+package jeongwookdongeun.checkeye;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class ColorBlind extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.color_blind_test, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/jeongwookdongeun/checkeye/MainActivity.java
+++ app/src/main/java/jeongwookdongeun/checkeye/MainActivity.java
@@ -1,10 +1,9 @@
 package jeongwookdongeun.checkeye;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -15,6 +14,13 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
     // step 1: add some instance
     private float mScale = 1f;
@@ -22,31 +28,56 @@ public class MainActivity extends AppCompatActivity {
     GestureDetector gestureDetector;
 
     ImageView resetBtn;
+    Button snellenChartBtn;
+    Button colorBlindBtn;
+
+    private AdView mAdview;
+
+    private int fragment = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        //step 2: create instance from GestureDetector(this step sholude be place into onCreate())
-        gestureDetector = new GestureDetector(this, new GestureListener());
 
-        setAnimation();
-
-        resetBtn = (ImageView)findViewById(R.id.resetBtn);
-        resetBtn.setOnClickListener(new View.OnClickListener(){
+        // snellenChart, colorBlind 메뉴 버튼 클릭 및 화면 전환
+        FragmentView();
+        snellenChartBtn = (Button)findViewById(R.id.snellenChartBtn);
+        colorBlindBtn = (Button)findViewById(R.id.colorBlindBtn);
+        snellenChartBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ImageView imageView = (ImageView)findViewById(R.id.snellenChart);
-                HorizontalScrollView hScrollView = (HorizontalScrollView)findViewById(R.id.horizontalScrollView);
-                ScrollView scrollView = (ScrollView)findViewById(R.id.scrollView);
-                imageView.clearAnimation();
-                scrollView.scrollTo(0,0);
-                hScrollView.scrollTo(0,0);
-                setAnimation();
+                fragment = 1;
+                FragmentView();
+                snellenChartBtn.setBackgroundResource(R.drawable.gray_solid_bold);
+                colorBlindBtn.setBackgroundResource(R.drawable.gray_solid);
             }
         });
+        colorBlindBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                fragment = 2;
+                FragmentView();
+                colorBlindBtn.setBackgroundResource(R.drawable.gray_solid_bold);
+                snellenChartBtn.setBackgroundResource(R.drawable.gray_solid);
+            }
+        });
+
+
+        // 애드몹
+        MobileAds.initialize(this, new OnInitializationCompleteListener() { //광고 초기화
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdview = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdview.loadAd(adRequest);
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER); //광고 사이즈는 배너 사이즈로 설정
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
     }
 
 
@@ -78,25 +109,55 @@ public class MainActivity extends AppCompatActivity {
         mScaleDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.SimpleOnScaleGestureListener()
         {
             @Override
-            public boolean onScale(ScaleGestureDetector detector)
-            {
-                float scale = 1 - detector.getScaleFactor();
+            public boolean onScale(ScaleGestureDetector detector) {
+                if (fragment == 1) {
+                    float scale = 1 - detector.getScaleFactor();
 
-                float prevScale = mScale;
-                mScale += scale;
+                    float prevScale = mScale;
+                    mScale += scale;
 
-                if (mScale < 1f) // Minimum scale condition:
-                    mScale = 1f;
+                    if (mScale < 1f) // Minimum scale condition:
+                        mScale = 1f;
 
-                if (mScale > 10f) // Maximum scale condition:
-                    mScale = 10f;
-                ScaleAnimation scaleAnimation = new ScaleAnimation(1f / prevScale, 1f / mScale, 1f / prevScale, 1f / mScale, detector.getFocusX(), detector.getFocusY());
-                scaleAnimation.setDuration(0);
-                scaleAnimation.setFillAfter(true);
-                ImageView layout =(ImageView) findViewById(R.id.snellenChart);
-                layout.startAnimation(scaleAnimation);
-                return true;
+                    if (mScale > 10f) // Maximum scale condition:
+                        mScale = 10f;
+                    ScaleAnimation scaleAnimation = new ScaleAnimation(1f / prevScale, 1f / mScale, 1f / prevScale, 1f / mScale, detector.getFocusX(), detector.getFocusY());
+                    scaleAnimation.setDuration(0);
+                    scaleAnimation.setFillAfter(true);
+                    ImageView layout = (ImageView) findViewById(R.id.snellenChart);
+                    layout.startAnimation(scaleAnimation);
+                    return true;
+                }
+                return false;
             }
         });
+    }
+
+    private void FragmentView(){
+        //FragmentTransaction 클래스로 프래그먼트를 사용합니다.
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragment){
+            case 1:
+                mScale = 1f;
+
+                // 첫번 째 프래그먼트 호출
+                SnellenChart snellenChart = new SnellenChart();
+                transaction.replace(R.id.frameLayout, snellenChart);
+                transaction.commit();
+
+                //step 2: create instance from GestureDetector(this step sholude be place into onCreate())
+                gestureDetector = new GestureDetector(this, new GestureListener());
+                setAnimation();
+
+                break;
+
+            case 2:
+                // 두번 째 프래그먼트 호출
+                ColorBlind colorBlind = new ColorBlind();
+                transaction.replace(R.id.frameLayout, colorBlind);
+                transaction.commit();
+                break;
+        }
     }
 }
